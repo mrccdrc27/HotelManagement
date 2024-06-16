af469262e9d0618e4c5ef0e293070d1e5ff26cac
CORE <CORE@CORE>
2024-06-16 19:23:34 +0800

    Add project files.

---
+++ WinFormsApp1/Room.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    public class Room
+    {
+        public int RoomID { get; set; }
+        public string? CategoryID { get; set; }
+        public int Session { get; set; }
+        
+        public virtual ObservableCollection <Category>
+
+    }
+}
